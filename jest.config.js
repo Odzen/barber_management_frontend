f11dfed8d00635d0f4632753b49c3322a7c48052
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-05-14 01:25:40 -0500

    cambio

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   setupFiles: ['./jest.setup.js'],
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage',
-  collectCoverageFrom: ['src/{container/sidebar, security/views, App.jsx}/*.jsx'],
+  collectCoverageFrom: ['src/**/*.jsx'],
   moduleNameMapper: {
     "^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
   }
