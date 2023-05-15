d0c1adece000aa7014cf260bcc215de5e6a97fdc
Odzen <jsebastian.va@gmail.com>
2023-05-15 18:21:15 -0500

    FIxed

--- src/security/views/login/index.jsx
+++ src/security/views/login/index.jsx
@@ -28,7 +28,7 @@ import './login.css'
 
 const LoginView = ({ setToken }) => {
   /* General states for receiving user data */
-  const API_URL = import.meta.env.VITE_API_URL
+  const API_URL = 'https://api.barbermanagment.live/api/auth/login'
   const [formCustomer] = Form.useForm()
   const [user, setUser] = useState(false)
   const [registeredUser, setRegisteredUser] = useState(false)
