4de5df69b61a9bb9c216ff479bf4c00d41a4df48
Santiago Sánchez Esquivel <86166934+SantiagoEsquivelHub@users.noreply.github.com>
2023-05-15 17:14:34 -0500

    change

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,7 +1,7 @@
 // import { render, screen, cleanup, fireEvent } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
-import App from '../App'
-import renderer from 'react-test-renderer'
+// import { MemoryRouter } from 'react-router-dom'
+// import App from '../App'
+// import renderer from 'react-test-renderer'
 
 describe('testing to App', () => {
   test('should return App Component', () => {
