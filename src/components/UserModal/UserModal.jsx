f55a919776d36d277feef16b99bc900819c53bd9
Santiago Sánchez <santiagosancheze8.1@gmail.com>
2023-06-05 15:11:18 -0500

    changes

--- src/components/UserModal/UserModal.jsx
+++ src/components/UserModal/UserModal.jsx
@@ -84,6 +84,8 @@ export const UserModal = ({
       description = 'Inténtalo de nuevo'
       openNotificationWithIcon(type, message, description)
     }
+    return
+
   }
 
   /* Function to update an user*/
